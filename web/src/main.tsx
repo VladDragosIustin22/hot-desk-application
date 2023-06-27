49fc305617ea30d0ea6d7567cb48345468500217
Vivien Knecht <knechtvivien@gmail.com>
2023-06-27 18:14:13 +0300

    main-screen design

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import SignUp from "./components/sign-up-page";
+import Header from "./components/main-screen";
 
 const router = createBrowserRouter([
   {
     path: "/sign-up-page",
     element: <SignUp />,
   },
+  {
+    path: "main-screen",
+    element: <Header />,
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
