b97c8578eaf44b2ee6a50ea972a959981540aaa6
Zotta317 <victor.zotta@gmail.com>
2023-07-26 18:03:04 +0300

    GetAllReservations

--- api/HotDeskApplicationApi/ModelView/ReservationView.cs
+++ api/HotDeskApplicationApi/ModelView/ReservationView.cs
@@ -2,13 +2,12 @@
 {
     public class RegistrationView
     {
-        public Guid ProfileID { get; set; }
         public DateTime ArrivalTime { get; set; }
         public DateTime LeavingTime { get; set; }
         public string? OfficaName { get; set; }
         public string? FloorName { get; set; }
         public string? DeskName { get; set; }
-        public string? Avatar { get; set; }
+        public byte[]? Avatar { get; set; }
         public string? ProfileRole { get; set; }
         public string? ProfileName { get; set; }
     }
--- api/HotDeskApplicationApi/Models/Profile.cs
+++ api/HotDeskApplicationApi/Models/Profile.cs
@@ -8,7 +8,7 @@ namespace HotDeskApplicationApi.Models
         public string? FirstName { get; set; } = default!;
 
         public string? LastName { get; set; } = default!;
-        public string? Avatar { get; set; } = default!;
+        public byte[]? Avatar { get; set; } = default!;
         public string? Role { get; set; } = default!;
         public string? NickName { get; set; } = default!;
         public string EmailAddress { get; set; } = default!;
