a58bef461aa82cc80f1969d1c9ab6f605c6641c3
vivienknecht <knechtvivien@gmail.com>
2023-08-08 15:19:01 +0300

    reserve a desk  for any user

---
+++ api/HotDeskApplicationApi/ModelView/AdminReservation.cs
@@ -0,0 +1,12 @@
+﻿namespace HotDeskApplicationApi.ModelView
+{
+    public class AdminReservation
+    {
+       public Guid UserID { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public DateTime LeavingTime { get; set; }
+        public Guid OfficeID { get; set; }
+        public Guid FloorID { get; set; }
+        public Guid DeskID { get; set; }
+    }
+}
